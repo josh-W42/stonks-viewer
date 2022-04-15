f3d7f0f66bcec8aeaedd32aff75f6bb7a3c4591c
josh-W42 <wilson.jd022@gmail.com>
2022-04-14 23:08:34 -0700

    added basic router with 3 routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import StonksViewer from './App';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <StonksViewer />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
