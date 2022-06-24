aadcf6466aa2c8426edf6f10ffaede9963b04f2f
josh-W42 <wilson.jd022@gmail.com>
2022-06-23 18:53:47 -0700

    Switched some basic components from primereact to mui

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import StonksViewer from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import 'primereact/resources/primereact.min.css';
-import 'primereact/resources/themes/lara-light-blue/theme.css';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
--- src/pages/landing/components/LandingPage/component.tsx
+++ src/pages/landing/components/LandingPage/component.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'primereact/button';
 import { StockSearchUnit } from '../../../../common/components/StockSearchUnit';
+import Button from '@mui/material/Button';
 
 export const LandingPageComponent: React.FunctionComponent = () => {
-  // An example method of using gql queries
-  // const ExchangeRates = () => {
-  //   const { loading, error, data } = useQuery<IBookResponse>(EXCHANGE_RATES);
-
-  //   if (loading) return <p>Loading...</p>;
-  //   if (error) return <p>An Error Occured</p>;
-
-  //   console.log(data);
-
-  //   return data?.books.map(({ title, author }) => (
-  //     <p key={title}>
-  //       {title}: {author}
-  //     </p>
-  //   ));
-  // };
-
   return (
     <div>
       <h1>HI THIS IS THE HOME PAGE</h1>
       <Link to={'/login'}>
-        <Button label="Login" className="p-button-outline p-button-text" />
+        <Button variant={'outlined'}>Login</Button>
       </Link>
       <Link to={'/signUp'}>
-        <Button label="Signup" className="p-button-outline p-button-text" />
+        <Button variant={'outlined'}>Signup</Button>
       </Link>
       <div className="col-12 md:col-4">
         <StockSearchUnit />
