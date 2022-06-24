4b00ad34612421d5b0b0d85b40f9b780281dd6c1
josh-W42 <wilson.jd022@gmail.com>
2022-06-23 18:54:50 -0700

    Created a basic component that searches for stocks and displays results in a dropdown fashion, ultimately leading to quote pages

--- src/common/components/StockSearchUnit/component.tsx
+++ src/common/components/StockSearchUnit/component.tsx
@@ -1,78 +1,18 @@
-import React, { useState } from 'react';
-import { AutoComplete } from 'primereact/autocomplete';
-import { IError, IGQLQueryResponse } from '../../types';
-import { IBasicMatchStock, IBasicSearchStocks } from './types';
-import { ProgressSpinner } from 'primereact/progressspinner';
+import { Card, CardActions, CardContent, Typography } from '@mui/material';
+import React from 'react';
+import { StockAutocomplete } from './components/StockAutoComplete';
 
-interface Props {
-  GetData: (input: string) => IGQLQueryResponse<IBasicSearchStocks>;
-}
-
-export const StockSearchUnitComponent: React.FunctionComponent<Props> = ({
-  GetData,
-}: Props) => {
-  const [input, setInput] = useState<string>('');
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [hasError, setHasError] = useState<boolean>(false);
-  const [matches, setMatches] = useState<IBasicMatchStock[]>([]);
-  const [errorMsg, setErrorMsg] = useState<IError | undefined>();
-
-  const searchStocks = (event: { query: string }) => {
-    console.log('Search was called');
-    const response = GetData(event.query);
-    if (response.isLoading) {
-      setIsLoading(response.isLoading);
-    }
-
-    if (response.hasError) {
-      setHasError(response.hasError);
-      setErrorMsg(response.errorObj);
-    }
-
-    if (response.data) {
-      setMatches(response.data.bestMatches);
-    } else {
-      setMatches([]);
-    }
-  };
-
-  if (isLoading) {
-    return (
-      <div>
-        <p>Loading...</p>
-        <ProgressSpinner
-          style={{ width: '50px', height: '50px' }}
-          strokeWidth="5"
-          fill="var(--surface-ground)"
-          animationDuration=".5s"
-        />
-      </div>
-    );
-  }
-
-  if (hasError) {
-    return (
-      <div>
-        <h1>An error has occured: {errorMsg?.message}</h1>
-      </div>
-    );
-  }
-
+export const StockSearchUnitComponent: React.FunctionComponent = () => {
   return (
-    <div>
-      <h1>This is the StockSearchUnit</h1>
-
-      <div>
-        <AutoComplete
-          value={input}
-          suggestions={matches}
-          completeMethod={searchStocks}
-          field="name"
-          onChange={(e) => {
-            setInput(e.value);
-          }}
-        />
-      </div>
-    </div>
+    <Card sx={{ minWidth: 275, maxWidth: 600 }}>
+      <CardContent>
+        <Typography variant={'h6'} component={'h1'}>
+          This is the StockSearchUnit
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <StockAutocomplete />
+      </CardActions>
+    </Card>
   );
 };
--- src/common/components/StockSearchUnit/container.tsx
+++ src/common/components/StockSearchUnit/container.tsx
@@ -1,28 +1,6 @@
-import { useQuery } from '@apollo/client';
 import React from 'react';
-import { IGQLQueryResponse } from '../../types';
 import { StockSearchUnitComponent } from './component';
-import { GET_STOCK_BASIC } from './queries';
-import { IBasicSearchStocks } from './types';
 
 export const StockSearchUnit: React.FunctionComponent = () => {
-  const GetStocks = (input: string): IGQLQueryResponse<IBasicSearchStocks> => {
-    console.log('The input was: ', input);
-    const { loading, error, data } = useQuery<IBasicSearchStocks>(
-      GET_STOCK_BASIC,
-      { variables: { input: input.trim() } }
-    );
-
-    if (loading) return { isLoading: true, hasError: false };
-    if (error)
-      return {
-        isLoading: false,
-        hasError: true,
-        errorObj: { message: 'An error has occurred when fetching stocks' },
-      };
-
-    return { isLoading: false, hasError: false, data };
-  };
-
-  return <StockSearchUnitComponent GetData={GetStocks} />;
+  return <StockSearchUnitComponent />;
 };
